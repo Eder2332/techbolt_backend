fcd3e8fd1461af773dfb4014d14490708c3790ed
Ederson <ederson.vilca@tecsup.edu.pe>
2026-06-21 22:26:14 -0500

    Corregir conexión IPv4 para Render

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
+const dns = require('dns');
 const app = require('./app');
 const { connectDatabase } = require('./config/database');
 const { syncUserModel } = require('./models/userModel');
 
 const PORT = process.env.PORT || 3000;
+
+if (typeof dns.setDefaultResultOrder === 'function') {
+  dns.setDefaultResultOrder(process.env.DNS_RESULT_ORDER || 'ipv4first');
+}
 
 async function startServer() {
   try {
